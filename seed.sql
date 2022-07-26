2e5df441a32447f77761d9de5173b67d90ec1b7f
Lucia Gil <luciagil5952@gmail.com>
2022-07-26 16:22:04 -0400

    Added schema and seed info

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (id, department_name)
+VALUES (1, "Engineering"),
+       (2, "Sales"),
+       (3, "Finance"),
+       (4, "Legal");
+       
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES (1, "Sales Lead", 100000 , ),
+       (2, "Sales"),
+       (3, "Finance"),
+       (4, "Legal");
