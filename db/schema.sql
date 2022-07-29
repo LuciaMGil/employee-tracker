e0a2ef8eb21fbeb8d4ba28c75f04700ee1ba2fce
Lucia Gil <luciagil5952@gmail.com>
2022-07-29 14:56:57 -0400

    Fixed schema added switch statement

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,33 @@ use company_db;
 
 create table department( 
     id INT PRIMARY KEY,
-    department_name VARCHAR(30) 
+    department_name VARCHAR(30),
+    PRIMARY KEY(id)
 );
 
 create table roles(
-    id INT PRIMARY KEY,
+    id INT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY(id)
 );
  
 create table employee( 
-    id INT PRIMARY KEY,
+    id INT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES manager(id),
+    PRIMARY KEY(id)
 );
 
-
+CREATE TABLE manager (
+  id INT,
+  managerName VARCHAR(30),
+  PRIMARY KEY (id)
+);
   
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -11,17 +12,14 @@ const connection = mysql.createConnection({
   );
 
 
-connection.query('SELECT * FROM department', function (err, results){
-    err ? console.log(err) : console.log(results);
-})
 
 promptQuestions = () => {
     return inquirer.prompt([
         {
         type: 'list',
         message: 'What can I help with?',
-        name: 'name',
-        choices: [ 'View all departments', 'View all roles, View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        name: 'choice',
+        choices: [ 'View all departments', 'View all roles' , 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
         }
     ])
 }
@@ -124,8 +122,68 @@ const updateEmployee = () => {
     })
 };
 
+const viewAllEmployees = () => {
+    // console.log(
+    //   boxen(
+    //     "                            - EMPLOYEES -                             ",
+    //     { padding: 1, borderStyle: "double" }
+    //   )
+    // );
+    const query = `SELECT 
+    employee.id AS "ID",
+    first_name AS "First Name",
+    last_name AS "Last Name",
+    role_id AS "Title",
+    FROM employee
+    `;
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log(
+        `-------------------------------------------------------------------------------`
+      );
+      promptQuestions();
+    });
+};
 
 promptQuestions()
 .then((data) => {
-    // Switch case and invoke functions
+    switch (data.choice) {
+        case "View all departments":
+            connection.query('SELECT * FROM department', function (err, results){
+                err ? console.log(err) : console.log(results);
+            })
+            break;
+        case "View all roles":
+            connection.query('SELECT * FROM roles', function (err, results){
+                err ? console.log(err) : console.log(results);
+            })
+            break;
+        case "View all employees":
+            viewAllEmployees();
+            // console.log(`View all employee`)
+            // connection.query('SELECT * FROM employee', function (err, results){
+            //     err ? console.log(err) : console.log(results);
+            // })
+            //  'Add an employee', 'Update an employee role'
+            break;
+        case "Add a department":
+            console.log(`Add a department`);
+            break;
+        case "Add a role":
+            console.log(`Add a role`);
+            break;
+        case "Add an employee":
+            console.log(`Add an employee`);
+            break;
+        case "Update an employee role":
+            console.log(`Update an employee role`);
+            break;
+        case "Exit":
+        
+        
+            console.log(`Done`);
+            break;
+    }
 });
